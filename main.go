9e663337ec6fe07c38496c2806f418e0e3424a8d
Julien DEROUET <juju509@users.noreply.github.com>
2019-08-08 14:46:00 +0200

    Update message
    -

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello My F* World, WWWWWWoooooooowwwww"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
